118b20f0aa5feb12ef297098d5b05487fc847276
Dennis Boon <dennis.boon96@gmail.com>
2019-03-04 11:18:11 +0100

    Added existing scripts from prototype and added Steam VR plugin

---
+++ Pro_V/Assets/_Scripts/SFXTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFXTriggers : MonoBehaviour
+{
+    AudioSource audioData;
+    public float volume = 1.0f;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.volume = volume;
+    }
+
+    public void GoalSound()
+    {
+        audioData.Play(0);
+    }
+}
---
+++ Pro_V/Assets/_Scripts/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Scoreboard : MonoBehaviour
+{
+    public TextMeshPro scoreText;
+    public TextMeshPro timeText;
+
+    public GoalMinigame scoreboard;
+
+    void FixedUpdate()
+    {
+        SetScoreboardText();
+    }
+
+    void SetScoreboardText()
+    {
+        scoreText.text = " " + scoreboard.score.ToString();
+        timeText.text = " " + scoreboard.time.ToString("f0");
+    }
+}
