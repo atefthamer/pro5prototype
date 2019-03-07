f56330a66ad676dde1b2f931e78d9f03ccd3d731
Dennis Boon <dennis.boon96@gmail.com>
2019-03-07 12:18:41 +0100

    Added character and arena model to Unity, added effect to target

--- Pro_V/Assets/_Scripts/Tutorial/Target_color.cs
+++ Pro_V/Assets/_Scripts/Tutorial/Target_color.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class Target_color : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material[] mats;
+    int value = 0;
+    int childNumber = 2;
+    [HideInInspector]
+    public Transform[] childs;
+    public GameObject[] childObjects;
+    private float timer = 0.05f;
+
     void Start()
     {
-        
+        childs = gameObject.GetComponentsInChildren<Transform>();
+        childObjects = new GameObject[childs.Length];
+
+        foreach (Transform tr in childs)
+        {
+            value++;
+            childObjects.SetValue(tr.gameObject, value - 1);
+        }
+
+        //Component[] renderers = this.GetComponentsInChildren(typeof(Renderer));
+        //foreach (Renderer childRenderer in renderers)
+        //{
+        //    childRenderer.material = mats[1];
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer <= 0.0f)
+        {
+            NextColor();
+            timer = 0.05f;
+        }
+
+        //childObjects[childNumber].GetComponent<Renderer>().material = mats[1];
+
+        //for (childNumber = 2; childNumber < 10; childNumber++)
+        //{
+        //    if (childNumber == childObjects.Length)
+        //    {
+        //        childNumber = 2;
+        //    }
+
+        //    Component[] renderers = this.GetComponentsInChildren(typeof(Renderer));
+        //    foreach(Renderer childRenderer in renderers)
+        //    {
+        //        childRenderer.material = mats[0];
+        //    }
+        //    childObjects[childNumber].GetComponent<Renderer>().material = mats[1];
+        //}
+    }
+
+    void NextColor()
+    {
+        Component[] renderers = this.GetComponentsInChildren(typeof(Renderer));
+        foreach(Renderer childRenderer in renderers)
+        {
+            childRenderer.material = mats[0];
+        }
+
+        childObjects[childNumber].GetComponent<Renderer>().material = mats[1];
+        childNumber++;
+
+        if (childNumber == childObjects.Length)
+        {
+            childNumber = 1;
+        }
     }
 }
