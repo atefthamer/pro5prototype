9fba0fa60186b90325be680eaccdde2ce24b27fa
Dennis Boon <dennis.boon96@gmail.com>
2019-03-12 11:42:22 +0100

    Organized some stuff, made a testscene for myself

---
+++ PRO5V3/Assets/_Scripts/Test_Scripts/Trigger_Minigame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Minigame : MonoBehaviour
+{
+    public Material[] mats;
+    private float timer = 10.0f;
+    private bool startCountdown = false;
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material = mats[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartMinigame();
+        }
+
+        if (startCountdown == true)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void StartMinigame()
+    {
+        gameObject.GetComponent<Renderer>().material = mats[1];
+        startCountdown = true;
+    }
+}
