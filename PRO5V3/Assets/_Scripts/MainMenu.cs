6552dd9843e503e0ce8536e7a38792cb8ffa208d
Tim <tim.philipp@hva.nl>
2019-03-25 11:56:04 +0100

    animated color change on Bowow top. charcacter in T-pose now

---
+++ PRO5V3/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart = true;
+    public bool isLoad = true;
+    public bool isPrefs = true;
+    public bool isQuit = true;
+    void OnMouseUp()
+    {
+        //if (isStart)
+        //{
+        //    Application.LoadLevel(1);
+        //}
+        //else
+        //{
+        //    Application.Quit();
+        //}
+        //if (isQuit)
+        //{
+            
+        //}
+        ChangeState(isStart, 1);
+    }
+
+    void ChangeState(bool state ,int x)
+    {
+        if (state)
+        {
+            Application.LoadLevel(x);
+        }
+        else
+        {
+            Application.Quit();
+        }
+       
+    }
+}
