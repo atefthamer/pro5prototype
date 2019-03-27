060535957d9ede9cd253b4882ea11dd8efa413de
Atef Thamer <atef.thamer@hva.nl>
2019-03-27 11:12:58 +0100

    Bowwow low height level, animations to animation directory

---
+++ PRO5V3/Assets/_Scripts/BowWow/TextToSpeech.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextToSpeech : MonoBehaviour
+{
+    public AudioSource _audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio = gameObject.GetComponent<AudioSource>();
+        StartCoroutine(DownloadTheAudio());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator DownloadTheAudio(){
+        string url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=nl&q=Vliegtuig";
+        WWW www = new WWW(url);
+        yield return www;
+        _audio.clip = www.GetAudioClip(false, true,AudioType.MPEG);
+        _audio.Play();
+      
+    }
+}
