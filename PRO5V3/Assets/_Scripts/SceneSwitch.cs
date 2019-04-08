36a6a73a634e51c15b90bc30229660a785b27335
Dennis Boon <dennis.boon96@gmail.com>
2019-04-08 16:25:24 +0200

    Auto stash before merge of "devdev" and "origin/devdev"

--- PRO5V3/Assets/_Scripts/SceneSwitch.cs
+++ PRO5V3/Assets/_Scripts/SceneSwitch.cs
@@ -1,11 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
 
-public class SceneSwitch : MonoBehaviour {
-    public void SceneSwitcher (int sceneIndex)
+public class SceneSwitch : UIElement
+{
+    [SerializeField]
+    private readonly GameObject player;
+
+    private void Start()
     {
-        SceneManager.LoadScene (sceneIndex);
+        Debug.Log(player);
     }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //ui = this.GetComponentInParent<SkeletonUIOptions>();
+        //sw = this.GetComponent<SceneSwitch>();
+        //mng = new SceneManager();
+    }
+
+    public void DestroyPlayer()
+    {
+        Destroy(player);
+        SceneManager.LoadScene(1);
+    }
+
+    protected override void OnButtonClick()
+    {
+        base.OnButtonClick();
+        //SceneManager.LoadScene()
+        //if(sw != null)
+        //{
+        //    sw.SceneSwitcher(1);
+        //}
+
+
+    }
+
+    //public void SceneSwitcher (int sceneIndex)
+    //{
+    //    Destroy(player);
+    //    SceneManager.LoadScene (sceneIndex);
+    //}
 }
