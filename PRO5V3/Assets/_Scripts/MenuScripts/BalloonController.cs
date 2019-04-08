123c275bef72022d369b7648f2d593ec08659653
Dennis Boon <dennis.boon96@gmail.com>
2019-04-08 11:53:27 +0200

    changes on switch_scene transition

---
+++ PRO5V3/Assets/_Scripts/MenuScripts/BalloonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour {
+
+    public float scaleFactor = 1.2f;
+    public float maxScale = 8;
+    public BalloonSoundScript sound;
+    public BLN_Rub_SFX bln_rub;
+
+    [SerializeField]
+    public bool hit = false;
+
+    public void BalloonScaler() {
+        transform.localScale *= scaleFactor;
+        bln_rub.PlaySound();
+        if (transform.localScale.x >= maxScale) {
+            sound.PlaySound();
+            Destroy(gameObject);
+        }
+    }
+}
