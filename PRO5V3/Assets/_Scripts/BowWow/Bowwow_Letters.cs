46c4f8c7c5fb854fd5df461672622e686c19480a
Dennis Boon <dennis.boon96@gmail.com>
2019-04-09 12:00:43 +0200

    Added zeppelin with scoreboard to BowWow

--- PRO5V3/Assets/_Scripts/BowWow/Bowwow_Letters.cs
+++ PRO5V3/Assets/_Scripts/BowWow/Bowwow_Letters.cs
@@ -9,25 +9,8 @@ public class Bowwow_Letters : MonoBehaviour
 
     public LoadLetters lettersGenerator;
 
-    //public string text = "";
     void Update()
     {
         lettersText.SetCharArray(lettersGenerator.wordSplit);
-        //if (lettersGenerator.useCharArray)
-        //{
-        //    for(int i = 0; i < lettersGenerator.wordSplit.Length; i++)
-        //    {
-        //        //lettersGenerator.wordSplit.ToString()
-        //        //text.Insert(i,lettersGenerator.wordSplit[i].ToString());
-        //        //text += lettersGenerator.wordSplit[i].ToString();
-        //        //=lettersGenerator.wordSplit[i]; 
-        //        //Debug.Log("LETTER ()()()() " + lettersGenerator.wordSplit[i].ToString());
-
-        //        //lettersText.SetText(lettersGenerator.wordSplit[i].ToString());
-        //    }
-        //    //Debug.Log("TEXT >> " + text);
-        //    //lettersText.SetText(lettersGenerator.wordSplit.ToString());
-        //    lettersText.SetCharArray(lettersGenerator.wordSplit);
-        //}      
     }
 }
---
+++ PRO5V3/Assets/_Scripts/BowWow/ZeppelinMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeppelinMovement : MonoBehaviour
+{
+    public GameObject point;
+    public Vector3 axis;
+    public float angle;
+
+    private void Update()
+    {
+        transform.RotateAround(point.transform.position, axis, angle);
+    }
+}
