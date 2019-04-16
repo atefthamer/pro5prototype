e31b723a7f92b2e7430bbd08df656a3a4c255d2a
Dennis Boon <dennis.boon96@gmail.com>
2019-04-16 15:50:15 +0200

    Made a pointer and added ability to shoot ball

--- PRO5V3/Assets/_Scripts/SlashCard/ShieldController.cs
+++ PRO5V3/Assets/_Scripts/SlashCard/ShieldController.cs
@@ -7,7 +7,18 @@ public class ShieldController : MonoBehaviour
     [HideInInspector]
     public GameObject currentProjectile = null;
 
+    public LineRenderer pointer;
+    public GameObject pointerEnd;
+    private GameObject shootingPoint;
+    private bool fired = false;
+    public float speed = 1.0f;
+
     public LauncherManager launcher;
+
+    private void Start()
+    {
+        shootingPoint = new GameObject("ShootingPoint");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,20 +26,38 @@ public class ShieldController : MonoBehaviour
         {
             currentProjectile = other.gameObject;
             currentProjectile.GetComponent<ProjectileController>().enabled = false;
+            this.transform.GetChild(1).gameObject.SetActive(true);
         }
     }
 
     private void Update()
     {
-        if (currentProjectile != null)
+        pointer.SetPosition(0, transform.position);
+        pointer.SetPosition(1, pointerEnd.transform.position);
+
+        if (currentProjectile != null && fired == false)
         {
             currentProjectile.transform.position = this.transform.GetChild(0).transform.position;
+        }
+        else if (currentProjectile != null && fired == true)
+        {
+            float shot = speed * Time.deltaTime;
+            currentProjectile.transform.position = Vector3.MoveTowards(currentProjectile.transform.position, shootingPoint.transform.position, shot);
+
+            if (Vector3.Distance(currentProjectile.transform.position, shootingPoint.transform.position) < 0.001f)
+            {
+                Destroy(currentProjectile);
+                currentProjectile = null;
+                fired = false;
+                launcher.NextLauncher();
+            }
         }
     }
 
     public void ShootProjectile()
     {
-        Destroy(currentProjectile);
-        launcher.NextLauncher();
+        shootingPoint.transform.position = pointerEnd.transform.position;
+        this.transform.GetChild(1).gameObject.SetActive(false);
+        fired = true;
     }
 }
