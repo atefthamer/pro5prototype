700aab41c32fd6750b4274eb6be4297b87bb3fcf
Dennis Boon <dennis.boon96@gmail.com>
2019-04-17 14:53:47 +0200

    Made it so targets move towards player when pair is made

--- PRO5V3/Assets/_Scripts/SlashCard/TargetController.cs
+++ PRO5V3/Assets/_Scripts/SlashCard/TargetController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
+    public TargetManager tMan;
+
     Component halo;
 
     private void Start()
@@ -11,11 +13,46 @@ public class TargetController : MonoBehaviour
         halo = GetComponent("Halo");  
     }
 
+    private void Update()
+    {
+        if (tMan.incorrect == true)
+        {
+            DisableHalo();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("projectile"))
+        if (other.gameObject.CompareTag("projectile") && tMan.firstTarget == null)
         {
+            tMan.firstTarget = this.gameObject;
+            Debug.Log("FIRST TARGET = " + tMan.firstTarget);
             halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
         }
+        else if (other.gameObject.CompareTag("projectile") && tMan.firstTarget != null && this.gameObject != tMan.firstTarget)
+        {
+            tMan.secondTarget = this.gameObject;
+            Debug.Log("SECOND TARGET = " + tMan.secondTarget);
+            tMan.targetsHit = true;
+        }
+
+        if (other.gameObject.CompareTag("Interactable") && this.gameObject == tMan.firstTarget)
+        {
+            Destroy(this.gameObject);
+            tMan.firstTarget = null;
+        }
+
+        if (other.gameObject.CompareTag("Interactable") && this.gameObject == tMan.secondTarget)
+        {
+            Destroy(this.gameObject);
+            tMan.secondTarget = null;
+        }
+    }
+
+    private void DisableHalo()
+    {
+        halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+        tMan.incorrect = false;
+        Debug.Log(tMan.incorrect);
     }
 }
