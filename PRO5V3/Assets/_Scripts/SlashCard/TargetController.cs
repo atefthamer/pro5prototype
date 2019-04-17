4918f8170df558fd370abe5145f33e035644a511
Dennis Boon <dennis.boon96@gmail.com>
2019-04-17 12:03:41 +0200

    Added collision to targets

---
+++ PRO5V3/Assets/_Scripts/SlashCard/TargetController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    Component halo;
+
+    private void Start()
+    {
+        halo = GetComponent("Halo");  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("projectile"))
+        {
+            halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+        }
+    }
+}
