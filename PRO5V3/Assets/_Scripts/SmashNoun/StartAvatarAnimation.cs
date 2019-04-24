c26f53c7488dd017851335375d4c97613fbdaaf9
Tim <tim.philipp@hva.nl>
2019-04-24 15:21:26 +0200

    animated avatar talking to the user

---
+++ PRO5V3/Assets/_Scripts/SmashNoun/StartAvatarAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAvatarAnimation : MonoBehaviour
+{
+
+    public Animator animator;
+    float InputX;
+    float InputY;
+
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+}
