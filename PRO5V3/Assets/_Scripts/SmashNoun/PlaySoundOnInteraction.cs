898b51f444b1984e15a57b9b8c0f6b5775c6cf59
Tim <tim.philipp@hva.nl>
2019-04-24 16:00:11 +0200

    Animation / sounds / Interaction set

---
+++ PRO5V3/Assets/_Scripts/SmashNoun/PlaySoundOnInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnInteraction : MonoBehaviour
+{
+
+    public AudioSource audioData;
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Interactable"))
+        {
+            audioData.Play(0);
+        }
+    }
+}
