feffc7c8129c160f83a33ecf1ed2ef47c522c8df
Dennis Boon <dennis.boon96@gmail.com>
2019-04-25 12:17:13 +0200

    Added test cards to SlashCard, fixed some bugs

---
+++ PRO5V3/Assets/_Scripts/SlashCard/SwordController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    public void ChargeSword()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void UnchargeSword()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
--- PRO5V3/Assets/_Scripts/SlashCard/TargetController.cs
+++ PRO5V3/Assets/_Scripts/SlashCard/TargetController.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
+    [HideInInspector]
     public TargetManager tMan;
+    [HideInInspector]
+    public LauncherManager lMan;
+    [HideInInspector]
+    public Transform lookPoint = null;
+
+    private void Start()
+    {
+        transform.LookAt(lookPoint);
+        transform.Rotate(new Vector3(0, 90, 0));
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,22 +24,28 @@ public class TargetController : MonoBehaviour
             tMan.firstTarget = this.gameObject;
             Debug.Log("FIRST TARGET = " + tMan.firstTarget);
             this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            //lMan.NextLauncher();
+            //Destroy(other);
         }
         else if (other.gameObject.CompareTag("projectile") && tMan.firstTarget != null && this.gameObject != tMan.firstTarget)
         {
             tMan.secondTarget = this.gameObject;
             Debug.Log("SECOND TARGET = " + tMan.secondTarget);
             tMan.targetsHit = true;
+            //lMan.NextLauncher();
+            //Destroy(other);
         }
 
         if (other.gameObject.CompareTag("Interactable") && this.gameObject == tMan.firstTarget && tMan.correct == true)
         {
+            tMan.firstHit = true;
             Destroy(this.gameObject);
             tMan.firstTarget = null;
         }
 
         if (other.gameObject.CompareTag("Interactable") && this.gameObject == tMan.secondTarget && tMan.correct == true)
         {
+            tMan.secondHit = true;
             Destroy(this.gameObject);
             tMan.secondTarget = null;
         }
