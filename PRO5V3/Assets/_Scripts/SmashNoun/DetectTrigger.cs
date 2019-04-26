bf877bf571fc8d020fc5779c479ce9ad7684d7a7
Atef <atefthamer@gmail.com>
2019-04-26 14:20:03 +0200

    Hammer Trigger collision

---
+++ PRO5V3/Assets/_Scripts/SmashNoun/DetectTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SmashNoun
+{
+
+
+    public class DetectTrigger : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Barrel"))
+            {
+                UnitDeathEventInfo udei = new UnitDeathEventInfo();
+                udei.EventDescription = "Unit " + gameObject.name + " has died.";
+                udei.UnitGO = gameObject;
+                Debug.Log("It just works " + udei.UnitGO.name);
+                EventSystem.Current.FireEvent(udei);
+            }
+        }
+    }
+
+}
