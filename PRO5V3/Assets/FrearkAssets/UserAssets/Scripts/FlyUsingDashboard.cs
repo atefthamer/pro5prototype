f35a0127b28b389139abe64c6941bba876f622e2
Dennis Boon <dennis.boon96@gmail.com>
2019-05-16 10:03:26 +0200

    Script changes zeppelin

--- PRO5V3/Assets/FrearkAssets/UserAssets/Scripts/FlyUsingDashboard.cs
+++ PRO5V3/Assets/FrearkAssets/UserAssets/Scripts/FlyUsingDashboard.cs
@@ -7,36 +7,50 @@ public class FlyUsingDashboard : MonoBehaviour
 	public GameObject Plane;
 	public GameObject Fs;
 
-
 	public float speedRoty = 50.0f;
 	public float speedRotx = 50.0f;
 	public float speed = 1.0f;
 
-	private float roty = 0.0f;
+    public float maxTiltAngle = 20.0f;
+    Vector3 curRot;
+    float maxX;
+    float minX;
+
+    private float roty = 0.0f;
 	private float rotx = 0.0f;
 	private float forwardSpeed = 0.0f;
 
 	void Start () 
 	{
-		
-	}
+        curRot = Plane.transform.eulerAngles;
+
+        maxX = curRot.x + maxTiltAngle;
+        minX = curRot.x - maxTiltAngle;
+        Debug.Log(maxX);
+        Debug.Log(minX);
+    }
 	
 
 	void Update () 
 	{
-		if(StartButton.EngineRunning == true)
+        if (StartButton.EngineRunning == true)
 		{
 			//Mathf.Clamp (transform.localPosition.x, -0.3f, 0.3f);
 			//Mathf.Clamp (transform.localPosition.z, -0.3f, 0.3f);
 
 			roty = (roty + (transform.localPosition.x * speedRoty)) * Time.deltaTime;
 			rotx = (rotx + (transform.localPosition.z * speedRotx)) * Time.deltaTime;
+            rotx = Mathf.Clamp(rotx, minX, maxX);
 
-			Plane.transform.Rotate (0.0f, roty, 0.0f, Space.World);
+            Plane.transform.Rotate (0.0f, roty, 0.0f, Space.World);
 			Plane.transform.Rotate (rotx, 0.0f, 0.0f, Space.Self);
 
 			forwardSpeed = (Fs.transform.localPosition.z * speed) * Time.deltaTime;
 			Plane.transform.Translate (0.0f, 0.0f, forwardSpeed, Space.Self);
-		}
+
+            //curRot.x = Mathf.Clamp(curRot.x, minX, maxX);
+
+            //Plane.transform.eulerAngles = curRot;
+        }
 	}
 }
--- PRO5V3/Assets/FrearkAssets/UserAssets/Scripts/LocalRigidBodyConstraint.cs
+++ PRO5V3/Assets/FrearkAssets/UserAssets/Scripts/LocalRigidBodyConstraint.cs
@@ -2,53 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LocalRigidBodyConstraint : MonoBehaviour 
+public class LocalRigidBodyConstraint : MonoBehaviour
 {
-	public GameObject StangVert;
-	public GameObject StangHor;
+    public GameObject StangVert;
+    public GameObject StangHor;
 
-	public float minX = -0.3f;
-	public float maxX = 0.3f;
-	public float minZ = -0.3f;
-	public float maxZ = 0.3f;
+    public float minX = -0.3f;
+    public float maxX = 0.3f;
+    public float minZ = -0.3f;
+    public float maxZ = 0.3f;
 
-	private float lockPos = 0.0f;
+    private float lockPos = 0.0f;
 
-	void Update () 
-	{
-		if(transform.localPosition.x < minX)
-		{
-			transform.localPosition = new Vector3(minX, lockPos, transform.localPosition.z);
-			GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f,0.0f);
-			GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f,0.0f);
-		}
-		else transform.localPosition = new Vector3 (transform.localPosition.x, lockPos, transform.localPosition.z);
+    void Update()
+    {
+        if (transform.localPosition.x < minX)
+        {
+            transform.localPosition = new Vector3(minX, lockPos, transform.localPosition.z);
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else transform.localPosition = new Vector3(transform.localPosition.x, lockPos, transform.localPosition.z);
 
-		if(transform.localPosition.x > maxX)
-		{
-			transform.localPosition = new Vector3(maxX, lockPos, transform.localPosition.z);
-			GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f,0.0f);
-			GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f,0.0f);
-		}
-		else transform.localPosition = new Vector3 (transform.localPosition.x, lockPos, transform.localPosition.z);
+        if (transform.localPosition.x > maxX)
+        {
+            transform.localPosition = new Vector3(maxX, lockPos, transform.localPosition.z);
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else transform.localPosition = new Vector3(transform.localPosition.x, lockPos, transform.localPosition.z);
 
-		if(transform.localPosition.z < minZ)
-		{
-			transform.localPosition = new Vector3(transform.localPosition.x, lockPos, minZ);
-			GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f,0.0f);
-			GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f,0.0f);
-		}
-		else transform.localPosition = new Vector3 (transform.localPosition.x, lockPos, transform.localPosition.z);
+        if (transform.localPosition.z < minZ)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, lockPos, minZ);
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else transform.localPosition = new Vector3(transform.localPosition.x, lockPos, transform.localPosition.z);
 
-		if(transform.localPosition.z > maxZ)
-		{
-			transform.localPosition = new Vector3(transform.localPosition.x, lockPos, maxZ);
-			GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f,0.0f);
-			GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f,0.0f);
-		}
-		else transform.localPosition = new Vector3 (transform.localPosition.x, lockPos, transform.localPosition.z);
+        if (transform.localPosition.z > maxZ)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, lockPos, maxZ);
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else transform.localPosition = new Vector3(transform.localPosition.x, lockPos, transform.localPosition.z);
 
-		StangVert.transform.localPosition = new Vector3(transform.localPosition.x, 0.0f, 0.0f);
-		StangHor.transform.localPosition = new Vector3(0.0f, 0.0f, transform.localPosition.z);
-	}
+        StangVert.transform.localPosition = new Vector3(transform.localPosition.x, 0.0f, 0.0f);
+        StangHor.transform.localPosition = new Vector3(0.0f, 0.0f, transform.localPosition.z);
+    }
 }
