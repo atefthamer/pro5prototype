2beeac16f62127037f0249d569cf1679ffc8b8cc
Dennis Boon <dennis.boon96@gmail.com>
2019-05-16 14:22:17 +0200

    Created some scripts for letter spawning

---
+++ PRO5V3/Assets/_Scripts/BoWow 2.0/LetterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterController : MonoBehaviour
+{
+    [HideInInspector]
+    public Transform lookPoint = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(lookPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PRO5V3/Assets/_Scripts/BoWow 2.0/LetterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterManager : MonoBehaviour
+{
+    public GameObject[] letters;
+    float radius = 5f;
+
+    [SerializeField]
+    Transform lookPoint = null;
+
+    void Start()
+    {
+        List<GameObject> spawnList = new List<GameObject>(letters);
+
+        for (int i = 0; i < 10; i++)
+        {
+            int randomIndex = Random.Range(0, spawnList.Count);
+            Debug.Log("INDEX: " + randomIndex);
+            Debug.Log("LOOP: " + i);
+            float angle = i * Mathf.PI * 2f / 10;
+            Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, 1.0f, Mathf.Sin(angle) * radius);
+            GameObject instance = Instantiate(spawnList[randomIndex], newPos, Quaternion.identity);
+            instance.name = instance.name.Replace("(Clone)", "").Trim();
+            instance.gameObject.GetComponent<LetterController>().lookPoint = lookPoint;
+            Debug.Log("Created: " + instance.name + " " + instance.GetInstanceID());
+        }
+    }
+}
