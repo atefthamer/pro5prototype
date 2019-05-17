a8dd2b48c07c165b2b59658a4799bcf36aad0f0c
Dennis Boon <dennis.boon96@gmail.com>
2019-05-17 13:24:03 +0200

    Scene transition to minigames

---
+++ PRO5V3/Assets/_Scripts/Zeppelin/CheckpointLookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointLookAt : MonoBehaviour
+{
+    public Transform zeppelin;
+
+    void Update()
+    {
+        transform.LookAt(zeppelin);
+        transform.Rotate(new Vector3(0, 270, 0));
+    }
+}
---
+++ PRO5V3/Assets/_Scripts/Zeppelin/IslandSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IslandSwitcher : MonoBehaviour
+{
+    public int index;
+    public GameObject player;
+    private bool destroy = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zeppelin"))
+        {
+            Debug.Log("Collision Detected");
+            SwitchScene();
+        }
+    }
+
+    private void SwitchScene()
+    {
+        Destroy(player);
+        destroy = true;
+
+        if (destroy == true)
+        {
+            destroy = false;
+            SceneManager.LoadScene(index);
+        }
+    }
+}
