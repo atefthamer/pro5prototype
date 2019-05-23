95e7c89969c95914c5928f0e6eb44d325f32cfcd
Dennis Boon <dennis.boon96@gmail.com>
2019-05-23 12:07:17 +0200

    Made progress on the animation, first part works TODO: transitioning between animations

--- PRO5V3/Assets/_Scripts/SmashWord/RabController.cs
+++ PRO5V3/Assets/_Scripts/SmashWord/RabController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class RabController : MonoBehaviour
 {
-    private Animation anim;
-    private Animation rab;
+    private Animator anim;
+    private Animator rab;
     public GameObject rabbit;
     public bool playani;
-    public List<AnimationClip> animclips = new List<AnimationClip>();
+    // public List<AnimationClip> animclips = new List<AnimationClip>();
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animation>();
-        rab = rabbit.GetComponent<Animation>();
+        anim = GetComponent<Animator>();
+        rab = rabbit.GetComponent<Animator>();
         playani = false;
     }
 
@@ -22,10 +22,11 @@ public class RabController : MonoBehaviour
     {
         if (playani == true)
         {
-            anim.clip = animclips[5];
-            rab.clip = animclips[0];
-            anim.Play();
-            rab.Play();
+            //anim.clip = animclips[5];
+            //rab.clip = animclips[0];
+            anim.Play("Ani_bundle_bunny");
+            rab.Play("Jumping Up");
+            playani = false;
         }
     }
     public void PlayAnimation()
