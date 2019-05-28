8325accd97e0f96e3d07723ca8300fc53007ca3b
Dennis Boon <dennis.boon96@gmail.com>
2019-05-28 11:12:10 +0200

    Possibly fixed issue with incorrect sound playing any time a target is hit in BoWow

--- PRO5V3/Assets/_Scripts/BoWow 2.0/LetterController.cs
+++ PRO5V3/Assets/_Scripts/BoWow 2.0/LetterController.cs
@@ -37,7 +37,12 @@ public class LetterController : MonoBehaviour
                 lMan.NextSentence();
             }
         }
-        else
+
+        else if (other.gameObject.CompareTag("projectile") && this.gameObject.name == "Dus" && lMan.sentence.text != "Mijn vriend en ik vervelen ons, ... gaan wij buitenspelen" ||
+            other.gameObject.CompareTag("projectile") && this.gameObject.name == "Maar" && lMan.sentence.text != "Ik moet naar bed, ... ik ben nog niet moe" ||
+            other.gameObject.CompareTag("projectile") && this.gameObject.name == "Want" && lMan.sentence.text != "Ik moet na school mijn huiswerk maken, ... ik wil geen straf" ||
+            other.gameObject.CompareTag("projectile") && this.gameObject.name == "En" && lMan.sentence.text != "Als ik groot ben wil ik trouwen, ... ik wil gaan reizen" ||
+            other.gameObject.CompareTag("projectile") && this.gameObject.name == "Of" && lMan.sentence.text != "Ik kom straks naar buiten, ... na het avond eten")
         {
             lMan.IncorrectSound();
         }
