5d02af30ceb6943b73dfd2aac53f07ce4a9483cf
Jowently <jowentlyjosephina@hotmail.com>
2019-06-07 13:25:10 +0200

    idfdk

---
+++ PRO5V3/Assets/SpawnLadder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLadder : MonoBehaviour
+{
+    public GameObject ladder;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            ladder.gameObject.SetActive(true);
+        }
+    }
+}
