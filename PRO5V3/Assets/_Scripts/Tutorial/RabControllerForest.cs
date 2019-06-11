0cf3214ff8eb98545c0593ffdb87848ce626af0e
Tim <tim.philipp@hva.nl>
2019-06-11 12:09:22 +0200

    added audio code firing in StartingPoint Forest lvl & Zeppelin cockpit

---
+++ PRO5V3/Assets/_Scripts/Tutorial/RabControllerForest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabControllerForest : MonoBehaviour
+{
+    private Animator rab;
+    public List<AudioClip> rabbitClips = new List<AudioClip>();
+
+    void Start()
+    {
+        rab = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
+        StartCoroutine(RabbitTalkSequence1());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"));
+        {
+            this.gameObject.GetComponent<AudioSource>().PlayOneShot(rabbitClips[0]);
+            rab.Play("Talking");
+        }
+    }
+
+    public IEnumerator RabbitTalkSequence1()
+    {
+        yield return new WaitForSeconds(3.233f);
+        this.gameObject.GetComponent<AudioSource>().PlayOneShot(rabbitClips[0]);
+    }
+}
