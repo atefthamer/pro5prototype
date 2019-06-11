c18b20c3841cf143c1eeb74e587350053a87847d
Tim <tim.philipp@hva.nl>
2019-06-11 10:41:28 +0200

    code firing sample rabbit explanation OnTriggerEnter in lvl Slashcard

---
+++ PRO5V3/Assets/_Scripts/SlashCard/RabControllerSlash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabControllerSlash : MonoBehaviour
+{
+    private Animator rab;
+    public List<AudioClip> rabbitClips = new List<AudioClip>();
+
+    void Start()
+    {
+        rab = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
+        StartCoroutine(RabbitTalkSequence1());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"));
+        {
+            StartCoroutine(RabbitTalkSequence1());
+            rab.Play("Talking");
+        }
+    }
+
+    public IEnumerator RabbitTalkSequence1()
+    {
+        yield return new WaitForSeconds(3.233f);
+        this.gameObject.GetComponent<AudioSource>().PlayOneShot(rabbitClips[0]);
+    }
+}
